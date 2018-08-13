8b9327f68267d3308a608ee84780495538ecde60
Uxio Fuentefria <uxio@gnosis.pm>
2018-08-13 11:27:26 +0200

    Add tests for serializers

--- django_eth/serializers.py
+++ django_eth/serializers.py
@@ -1,5 +1,6 @@
 import logging
 
+from django.utils.translation import ugettext_lazy as _
 from ethereum.utils import checksum_encode
 from hexbytes import HexBytes
 from rest_framework import serializers
@@ -41,6 +42,23 @@ class EthereumAddressField(serializers.Field):
 
 
 class HexadecimalField(serializers.Field):
+    """
+    Serializes hexadecimal values starting by `0x`. Empty values should be None or just `0x`.
+    """
+
+    default_error_messages = {
+        'invalid': _('{value} is not an hexadecimal value.'),
+        'blank': _('This field may not be blank.'),
+        'max_length': _('Ensure this field has no more than {max_length} hexadecimal chars (not counting 0x).'),
+        'min_length': _('Ensure this field has at least {min_length} hexadecimal chars (not counting 0x).'),
+    }
+
+    def __init__(self, **kwargs):
+        self.allow_blank = kwargs.pop('allow_blank', False)
+        self.max_length = kwargs.pop('max_length', None)
+        self.min_length = kwargs.pop('min_length', None)
+        super().__init__(**kwargs)
+
     def to_representation(self, obj):
         if not obj:
             return '0x'
@@ -48,12 +66,33 @@ class HexadecimalField(serializers.Field):
             return obj.hex()
 
     def to_internal_value(self, data):
-        if not data or data == '0x':
-            return None
+        data = data.strip()  # Trim spaces
+        if data.startswith('0x'):  # Remove 0x prefix
+            data = data[2:]
+
+        if not data:
+            if self.allow_blank:
+                return None
+            else:
+                self.fail('blank')
+
+        data_len = len(data)
+        if self.min_length and data_len < self.min_length:
+            self.fail('min_length', min_length=data_len)
+        elif self.max_length and data_len > self.max_length:
+            self.fail('max_length', max_length=data_len)
+
         try:
             return HexBytes(data)
         except ValueError:
-            raise ValidationError("%s is not hexadecimal" % data)
+            self.fail('invalid', value=data)
+
+
+class Sha3HashField(HexadecimalField):
+    def __init__(self, **kwargs):
+        kwargs['max_length'] = 64
+        kwargs['min_length'] = 64
+        super().__init__(**kwargs)
 
 
 # ================================================ #
--- django_eth/tests/test_serializers.py
+++ django_eth/tests/test_serializers.py
@@ -1,14 +1,61 @@
-from django.core.exceptions import ValidationError
 from django.test import TestCase
+from ethereum.utils import sha3
+from hexbytes import HexBytes
+from rest_framework import serializers
 
-from ..validators import validate_checksumed_address
-from .factories import get_eth_address_with_key
+from ..serializers import HexadecimalField, Sha3HashField
 
 
-class TestValidators(TestCase):
-    def test_checksum_address_validator(self):
-        eth_address, eth_key = get_eth_address_with_key()
+class HexadecimalSerializerTest(serializers.Serializer):
+    value = HexadecimalField()
 
-        self.assertIsNone(validate_checksumed_address(eth_address))
 
-        self.assertRaises(ValidationError, validate_checksumed_address, eth_address.lower())
+class HexadecimalBlankSerializerTest(serializers.Serializer):
+    value = HexadecimalField(allow_blank=True)
+
+
+class HexadecimalNullSerializerTest(serializers.Serializer):
+    value = HexadecimalField(allow_null=True)
+
+
+class Sha3HashSerializerTest(serializers.Serializer):
+    value = Sha3HashField()
+
+
+class TestSerializers(TestCase):
+    def test_hexadecimal_field(self):
+        serializer = HexadecimalBlankSerializerTest(data={'value': '0x'})
+        self.assertTrue(serializer.is_valid())
+        self.assertIsNone(serializer.validated_data['value'])
+
+        serializer = HexadecimalBlankSerializerTest(data={'value': None})
+        self.assertFalse(serializer.is_valid())
+
+        serializer = HexadecimalNullSerializerTest(data={'value': None})
+        self.assertTrue(serializer.is_valid())
+        self.assertIsNone(serializer.validated_data['value'])
+
+        value = '0xabc'
+        serializer = HexadecimalSerializerTest(data={'value': value})
+        self.assertTrue(serializer.is_valid())
+        self.assertEqual(serializer.validated_data['value'], HexBytes(value))
+
+        value = 'abc'
+        serializer = HexadecimalSerializerTest(data={'value': value})
+        self.assertTrue(serializer.is_valid())
+        self.assertEqual(serializer.validated_data['value'], HexBytes(value))
+
+    def test_hash_serializer_field(self):
+        value = sha3('test').hex()
+        serializer = Sha3HashSerializerTest(data={'value': value})
+        self.assertTrue(serializer.is_valid())
+        self.assertEqual(serializer.validated_data['value'], HexBytes(value))
+
+        # Hash with one more character - Must be 32 bytes
+        serializer = Sha3HashSerializerTest(data={'value': value + 'a'})
+        serializer.is_valid()
+        self.assertFalse(serializer.is_valid())
+
+        # Hash with one less character - Must be 32 bytes
+        serializer = Sha3HashSerializerTest(data={'value': value[:-1]})
+        self.assertFalse(serializer.is_valid())
